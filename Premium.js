9e543b2a58185b1b633cfcb8ed2f3dd2bca40e9d
TheRealJoeFriel <jfriel46@gmail.com>
2018-11-27 20:34:26 -0500

    added premium page

---
+++ Premium.js
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name = "viewport" content = "width=device-width, initial-scale=1">
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<title>Upgrade to Premium!</title>
+		<!DOCTYPE html>
+	</head>
+	<style>
+		.logoWrapper {
+			margin: 0 auto;
+			text-align: center;
+		}
+		.logo {
+			width: 20%;
+		}
+		.title {
+			text-align: center;
+			font-weight: bold;
+			font-size: 3em;
+		}
+		.inputWrapper {
+			text-align: center;
+		}
+	</style>
+	<body>
+		<form action="" method="post" class="form-horizontal">
+			<div class="logoWrapper">
+				<img src="PremiumLogo.png" class="logo" alt="Flint logo">
+			</div>
+			<div class="title">
+				Upgrade to Premium! <br>
+			</div>
+			<div>
+				Upgrading to Flint Premium gives you unlimited match opportunities, priority status, as well as no Ads!
+			</div>
+			<div class="inputWrapper">
+				<strong>Credit Card:</strong><br>
+				<input type="text" name="username" class="col-md-offset-3 col-md-6">
+			</div>
+			<br><br>
+			<div class="inputWrapper">
+				<strong>Expiration:</strong><br>
+				<input type="text" name="username" class="col-md-offset-3 col-md-3">
+			</div>
+			<br><br>
+			<div class="inputWrapper">
+				<strong>CVV:</strong><br>
+				<input type="text" name="username" class="col-md-offset-3 col-md-3">
+			</div>
+			<br><br>
+
+
+	</body>
+</html>
